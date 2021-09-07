4da71e2a2a97ceaeeddd1174a2378b2d00480e3d
rijal <jeryzal333@gmail.com>
2021-09-07 08:47:11 +0700

    fix bug weather

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -146,188 +146,185 @@
               <p>Gambar awan</p>
               <p> {{ weather.weather[0].description }}</p>
             </div>
-            <div v-else>
-              <div 
-                class="bg-blue-100 animate-pulse bg-opacity-50 rounded mb-2"
-                style="height:35px; background: rgba(140,226,255,1) 39%;"
-              >
-                <p />
-              </div>
-              <div 
-                class="bg-blue-100 animate-pulse bg-opacity-50 rounded mb-2"
-                style="height:35px; background: rgba(140,226,255,1) 39%;"
-              >
-                <p />
-              </div>
-              <div 
-                class="bg-blue-100 animate-pulse bg-opacity-50 rounded mb-2"
-                style="height:35px; background: rgba(140,226,255,1) 39%;"
-              >
-                <p />
-              </div>
-              <div 
-                class="bg-blue-100 animate-pulse bg-opacity-50 rounded mb-2"
-                style="height:35px; background: rgba(140,226,255,1) 39%;"
-              >
-                <p />
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- End of activity -->
 
-        <!-- Futured work -->
-        <div class="mt-6 p-4 bg-white px-6">
-          <div class="border-b pb-6">
-            <h1 class="mt-2 mb-6 text-center text-3xl font-bold">
-              My Skill 
-              <font-awesome-icon icon="trophy" />
-            </h1>
-            <div class="md:grid grid-cols-2 gap-6">
-              <div class="row">
-                <div class="col-auto">
-                  <p class="">
-                    <font-awesome-icon icon="code" /> HTML
-                  </p>
-                  <p>
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star-half-alt" />
-                    <i class="far fa-star" />
-                  </p>
-                </div>
-                <div class="col-auto">
-                  <p class="">
-                    <i class="fab fa-css3" /> CSS
-                  </p>
-                  <p>
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star-half-alt" />
-                    <i class="far fa-star" />
-                  </p>
-                </div>
-                <div class="col-auto">
-                  <p class="">
-                    <font-awesome-icon :icon="['fab', 'js']" /> JavaScript
-                  </p>
-                  <p>
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star-half-alt" />
-                    <i class="far fa-star" />
-                  </p>
-                </div>
-                <div class="col-auto">
-                  <p class="">
-                    <i class="fab fa-bootstrap" /> Bootstrap
-                  </p>
-                  <p>
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star-half-alt" />
-                    <i class="far fa-star" />
-                  </p>
-                </div>
-                <div class="col-auto">
-                  <p class="w3css">
-                    {W3} W3 css
-                  </p>
-                  <p>
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star" />
-                    <font-awesome-icon icon="star-half-alt" />
-                    <i class="far fa-star" />
-                  </p>
-                </div>
+            <!-- Skeleton loading -->
+
+            <div v-else>
+              <div
+                style="height:75px; background: rgba(140,226,255,1) 39%;"
+                class="rounded animate-pulse text-center font-bold mb-2"
+              >
+                <span />
+              </div>
+              <div
+                style="height:65px; background: rgba(140,226,255,1) 39%;"
+                class="rounded animate-pulse text-center font-bold mb-2"
+              >
+                <span />
+              </div>
+              <div
+                style="height:60px; background: rgba(140,226,255,1) 39%;"
+                class="rounded animate-pulse text-center font-bold"
+              >
+                <span />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <!-- Main -->
-      <footer class="bg-white">
-        <div class="flex mt-20 px-10 items-center justify-around max-md">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-10 text-gray-800 w-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-              />
-            </svg>
-          </span>
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-10 textgray-800e w-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 8l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z"
-              />
-            </svg>
-          </span>
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-10 text-gray-800 w-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-              />
-            </svg>
-          </span>
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-10 text-gray-800 w-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          </span>
+      <!-- End of activity -->
+
+      <!-- Futured work -->
+      <div class="mt-6 p-4 bg-white px-6">
+        <div class="border-b pb-6">
+          <h1 class="mt-2 mb-6 text-center text-3xl font-bold">
+            My Skill 
+            <font-awesome-icon icon="trophy" />
+          </h1>
+          <div class="md:grid grid-cols-2 gap-6">
+            <div class="row">
+              <div class="col-auto">
+                <p class="">
+                  <font-awesome-icon icon="code" /> HTML
+                </p>
+                <p>
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star-half-alt" />
+                  <i class="far fa-star" />
+                </p>
+              </div>
+              <div class="col-auto">
+                <p class="">
+                  <i class="fab fa-css3" /> CSS
+                </p>
+                <p>
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star-half-alt" />
+                  <i class="far fa-star" />
+                </p>
+              </div>
+              <div class="col-auto">
+                <p class="">
+                  <font-awesome-icon :icon="['fab', 'js']" /> JavaScript
+                </p>
+                <p>
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star-half-alt" />
+                  <i class="far fa-star" />
+                </p>
+              </div>
+              <div class="col-auto">
+                <p class="">
+                  <i class="fab fa-bootstrap" /> Bootstrap
+                </p>
+                <p>
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star-half-alt" />
+                  <i class="far fa-star" />
+                </p>
+              </div>
+              <div class="col-auto">
+                <p class="w3css">
+                  {W3} W3 css
+                </p>
+                <p>
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star" />
+                  <font-awesome-icon icon="star-half-alt" />
+                  <i class="far fa-star" />
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <p class="text-center my-10 text-lg">
-          Copyright ©2020 All rights reserved
-        </p>
-      </footer>
+      </div>
     </div>
+    <!-- Main -->
+    <footer class="bg-white">
+      <div class="flex mt-20 px-10 items-center justify-around max-md">
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-10 text-gray-800 w-10"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+            />
+          </svg>
+        </span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-10 textgray-800e w-10"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M16 8l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z"
+            />
+          </svg>
+        </span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-10 text-gray-800 w-10"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+            />
+          </svg>
+        </span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-10 text-gray-800 w-10"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </span>
+      </div>
+      <p class="text-center my-10 text-lg">
+        Copyright ©2020 All rights reserved
+      </p>
+    </footer>
   </div>
 </template>
 <script>
@@ -355,7 +352,14 @@ export default {
             this.weather = ""
             this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=15d44f1a44552bfddc3a735c06c66344&units=metric`)
             .then( (response) => this.weather = response.data)
-            .catch( (error) => this.weather = { name: "Not Found", sys: { country: ""}, main: {temp: ""}, weather: [ {description: ""} ] })
+            .catch( (error) => this.weather = {
+              name: "Not found",
+              sys: { country: ""},
+              main: {temp: ""},
+              weather: [
+                { description: "" }
+              ]
+            })
             this.city = city
         }
     },
