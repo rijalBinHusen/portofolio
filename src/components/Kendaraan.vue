dfc23efbbbda32373e11412dde5eb845049da6f6
rijal <jeryzal333@gmail.com>
2021-09-30 07:38:44 +0700

    Vehicles finish

--- src/components/Kendaraan.vue
+++ src/components/Kendaraan.vue
@@ -1,12 +1,120 @@
 <template>
-    <h1>Kendaraaan saya</h1>
+    <div class="grid text-center h-full md:w-7/12 md:m-auto">
+        <h1 class="text-3xl my-8">Daftar kendaraan</h1>
+        <div v-if="kendaraan.hasOwnProperty('GJJBN')">
+            <div
+                class="text-white mb-2 text-center text-sm md:text-xl px-4 py-4 border rounded-lg bg-opacity-50 bg-black cursor-pointer w-auto mx-5"
+                v-for="gdg in Object.keys(kendaraan).slice(0, 6)"
+                :key="gdg"
+            >
+                <div @click="toggle(gdg)">
+                    <span class="mr-2 font-bold">{{ gdg }}</span>
+                    <span v-if="show !== gdg">
+                        <span class="mr-2">
+                            | Load:
+                            <b>{{ kendaraan.progress[gdg] }}</b>
+                        </span>
+                        <span class="mr-2">
+                            Queue:
+                            <b>{{ kendaraan.queue[gdg] }}</b>
+                        </span>
+                        <span class="mr-2">Outsand: 9</span>
+                    </span>
+                </div>
+                <transition name="trans">
+                    <div v-if="show === gdg" class="w-auto md:mx-5">
+                        <!-- Progress -->
+                        <div class="block border-2 mt-2 border-opacity-50 rounded-lg">
+                            <span
+                                @click="child = 'progress'"
+                                class="block mt-2 py-3 m-2 bg-white bg-opacity-30"
+                            >Progress: {{ kendaraan.progress[gdg] }}</span>
+                            <!-- List of vehicles -->
+                            <transition name="trans">
+                                <div v-if="show === gdg && child === 'progress'">
+                                    <div v-for="prog in kendaraan[gdg]">
+                                        <span
+                                            class="block border-2 py-3 m-2"
+                                            v-if="prog.flag == 2"
+                                        >{{ prog.nopol + ' - ' + prog.nodo }}</span>
+                                    </div>
+                                </div>
+                            </transition>
+                            <!-- End of List of vehicles -->
+                        </div>
+                        <!-- End of progress -->
+
+                        <!-- Queue -->
+                        <div class="block border-2 mt-2 border-opacity-50 rounded-lg">
+                            <span
+                                @click="child = 'queue'"
+                                class="block mt-2 py-3 m-2 bg-white bg-opacity-30"
+                            >Queue: {{ kendaraan.queue[gdg] }}</span>
+                            <!-- LIST OF VEHICLES -->
+                            <transition name="trans">
+                                <div v-if="show === gdg && child === 'queue'">
+                                    <div v-for="prog in kendaraan[gdg]">
+                                        <span
+                                            class="block border-2 py-3 m-2"
+                                            v-if="prog.flag == 1"
+                                        >{{ prog.nopol + ' - ' + prog.nodo }}</span>
+                                    </div>
+                                </div>
+                            </transition>
+                            <!-- END OF LIST VEHICLES -->
+                        </div>
+                        <!-- End of queue -->
+                        <div class="block border-t-2 mt-2 border-opacity-50">Outstand: 9</div>
+                    </div>
+                </transition>
+            </div>
+        </div>
+        <Skeleton v-else :rows="6" />
+    </div>
 </template>
 
 <script>
+import Skeleton from "./SkeletonLoading.vue"
 export default {
     name: "Kendaraan",
+    components: {
+        Skeleton
+    },
+    data() {
+        return {
+            show: false,
+            child: false,
+            kendaraan: {},
+        }
+    },
+    methods: {
+        toggle(gdg) {
+            if (this.show == gdg) this.show = false
+            else this.show = gdg
+            this.child = false
+        }
+    },
     mounted() {
-        this.$store.dispatch("Kendaraan/kendaraan")
-    }
+        this.$store.dispatch("Kendaraan/kendaraan").then(() => {
+            this.kendaraan = this.$store.getters["Kendaraan/kendaraan"]
+        })
+    },
+    // computed: {
+    //     kendaraan() {
+    //         return this.$store.getters["Kendaraan/kendaraan"]
+    //     }
+    // },
 }
 </script>
+
+<style scoped>
+.trans-enter-active,
+.trans-leave-active {
+    transition: opacity 1s ease;
+}
+
+.trans-enter-from,
+.trans-leave-to {
+    opacity: 0;
+}
+</style>
--- src/store/module/kendaraan.js
+++ src/store/module/kendaraan.js
@@ -2,22 +2,97 @@ import axios from "axios";
 
 const kendaraan = {
   namespaced: true,
-  state: {},
-  mutations: {},
+  state: {
+    vehicles: [],
+  },
+  mutations: {
+    kendaraan(state, value) {
+      state.vehicles = value
+      console.log(value)
+    }
+  },
   actions: {
     kendaraan({ commit, dispatch }) {
-      axios
+      return new Promise((resolve) => {
+        axios
         .get("http://localhost:5000/")
         .then((response) => {
-          // ("parse", response.data);
-          console.log(response.data);
+          commit("kendaraan", response.data)
+          resolve()
         })
         .catch((error) => {
           console.log(error);
         });
+      })
     },
   },
-  getters: {},
+  getters: {
+    kendaraan(state){
+      let result = {
+        GJJBN: [],
+        GJBC: [],
+        GJDP: [],
+        GJST: [],
+        GJCC: [],
+        GJH3: [],
+        queue: {
+          GJJBN: 0,
+          GJBC: 0,
+          GJDP: 0,
+          GJST: 0,
+          GJCC: 0,
+          GJH3: 0,
+        },
+        progress: {
+          GJJBN: 0,
+          GJBC: 0,
+          GJDP: 0,
+          GJST: 0,
+          GJCC: 0,
+          GJH3: 0,
+        },
+      }
+      state.vehicles.slice(0, -1).forEach((val) => {
+        // GJJBN
+        if(val.location.toLowerCase().indexOf("jabon") > -1) {
+          result.GJJBN.push(val)
+          if(val.flag == 2) result.progress.GJJBN = result.progress.GJJBN + 1
+          else result.queue.GJJBN = result.queue.GJJBN + 1
+        } 
+        // GJBC
+        else if(val.location.toLowerCase().indexOf("gjbc") > -1) {
+          result.GJBC.push(val)
+          if(val.flag == 2) result.progress.GJBC = result.progress.GJBC+ 1
+          else if(val.flag == 1) result.queue.GJBC = result.queue.GJBC+ 1
+        } 
+        // GJDP
+        else if(val.location.toLowerCase().indexOf("depan") > -1) {
+          result.GJDP.push(val)
+          if(val.flag == 2) result.progress.GJDP = result.progress.GJDP+ 1
+          else if(val.flag == 1) result.queue.GJDP = result.queue.GJDP+ 1
+        }
+        // GJST
+        else if(val.location.toLowerCase().indexOf("central") > -1) {
+          result.GJST.push(val)
+          if(val.flag == 2) result.progress.GJST = result.progress.GJST+ 1
+          else if(val.flag == 1) result.queue.GJST = result.queue.GJST+ 1
+        }
+        // GJCC
+        else if(val.location.toLowerCase().indexOf("gjcc") > -1) {
+          result.GJCC.push(val)
+          if(val.flag == 2) result.progress.GJCC = result.progress.GJCC+ 1
+          else if(val.flag == 1) result.queue.GJCC = result.queue.GJCC+ 1
+        }
+        // GJH3
+        else if(val.location.toLowerCase().indexOf("hall") > -1) {
+          result.GJH3.push(val)
+          if(val.flag == 2) result.progress.GJH3 = result.progress.GJH3+ 1
+          else if(val.flag == 1) result.queue.GJH3 = result.queue.GJH3+ 1
+        }
+      })
+      return result
+    },
+  },
 };
 
 export default kendaraan;
