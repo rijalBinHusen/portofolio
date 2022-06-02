e9874db2e0a7269a1266303c07c1f73809de1186
rijal <jeryzal333@gmail.com>
2022-06-02 20:37:31 +0700

    Add event listener when window resize

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,12 +12,12 @@
       </button>
       <!-- Burger button -->
     </div>
-    <nav class="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
+    <nav v-if="!mobileView && mobileNav" class="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
 
       <span v-for="link in links" :key="link.id" :class="['cursor-pointer block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline', active === link.id ? 'bg-gray-200' : '' ]" @click="active = link.id">
           {{ link.judul }}
       </span>
-      
+
     </nav>
   </div>
 </div>
@@ -35,10 +35,26 @@ export default {
           { id: 4, judul: "About"},
         ],
         active: 0,
+        mobileView: false,
+        mobileNav: true,
       }
     },
-    updated() {
-      console.log(this.active, "active")
-    }
+    methods: {
+      checkScreen() {
+        if(window.innerWidth <= 750) {
+            this.mobileView = true;
+            this.mobileNav = false
+            return
+        }
+            this.mobileView = false;
+            this.mobileNav = true
+      },
+    },
+    created() {
+      // add event listener to window on resize
+      window.addEventListener("resize", this.checkScreen);
+      // run this function after component created
+      this.checkScreen()
+    },
 }
 </script>
