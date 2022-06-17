5c3098309f87a933ab66d147d3db8cb253f67eaf
rijalBinHusen <67273416+rijalBinHusen@users.noreply.github.com>
2022-06-17 14:09:26 +0700

    Create hero component

---
+++ Hero.vue
@@ -0,0 +1,21 @@
+<template>
+
+
+	  <div class="break-words container md:mx-48 mt-32 mx-4 w-11/12">
+      <h1 class="text-5xl">Hello there! Im</h1>
+      <p class="font-bold mb-2 text-3xl">Rijal bin Husen</p>
+      <p class="mb-3">I love everything about innovation, especially innovations that benefit a lot of people.</p>
+      <p>That's why I love coding, because a combination of several codes can create awesome applications, and awesome applications can be benefit for many people.</p>
+      <section class="flex mt-8">
+        <button class="bg-opacity-50 bg-white block border-r-0 cursor-pointer font-bold font-mono hover:bg-gray-200 leading-tight mr-2 px-3 py-2 rounded-l rounded-r">Hire me!</button>
+        <button class="bg-opacity-50 bg-white block border-r-0 cursor-pointer font-bold font-mono hover:bg-gray-200 leading-tight px-3 py-2 rounded-l rounded-r">Get portofolio</button>
+      </section>
+	</div>
+
+</template>
+
+<script>
+  export default {
+    name: "Hero",
+  }
+</script>
