4f2a394ab506732d9a4d1d03e41668a9933d0479
rijal <jeryzal333@gmail.com>
2022-06-21 21:14:41 +0700

    adding index posts

---
+++ src/posts/index.js
@@ -0,0 +1,8 @@
+const posts = [
+  {
+    id: "html-basic",
+    date: "19-Des-2020",
+    title: "html basic",
+    description: "Something you may need to copy paste it, or just to read it",
+  },
+];
